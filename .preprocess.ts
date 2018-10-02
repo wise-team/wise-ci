b90b22c6969db426a3e3f0e24169ec8f886ada3e
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-10-02 20:04:13 +0200

    ci: add preprocess script

---
+++ .preprocess.ts
@@ -0,0 +1,9 @@
+// this file is loaded by wise-ci preprocessor
+
+export const hooks = [];
+export const data = {
+    repository: {
+        name: "wise-ci"
+    }
+};
+export const excludes = [ "js-template-hook.ts" ];
