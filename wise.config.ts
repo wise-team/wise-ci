4f5e94b18e7615a9b3a025049a67222601ebece9
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-10-08 22:24:00 +0200

    feat: add list of working apis (tested carefoully)

--- wise.config.ts
+++ wise.config.ts
@@ -10,6 +10,33 @@ export class Config {
         homepage: "https://wise.vote/"
     };
 
+    steem = {
+        minimalApiBlockchainVersion: "0.20.5",
+        minimalApiHardforkVersion: "0.20.0",
+        apis: [
+            { url: "https://api.steemit.com", get_block: true },
+            { url: "https://steemd.minnowsupportproject.org", get_block: true },
+            { url: "https://rpc.buildteam.io", get_block: true },
+            { url: "https://rpc.steemliberator.com", get_block: true }, /* http (plain) also works */
+            // unstable: { url: "wss://rpc.steemviz.com", get_block: true }, /* ws (plain) also works */
+            { url: "https://steemd.privex.io", get_block: true },
+            // worked, but stopped to work: { url: "wss://steemd.privex.io", get_block: true }, /* all protocols work: ws, wss, http, https */
+
+            // check! { url: "wss://gtg.steem.house:8090", get_block: true },
+            // check! { url: "wss://api.steem.house:8090", get_block: true },
+            // check! { url: "https://api.steem.house", get_block: true },
+            // check! { url: "wss://rpc.curiesteem.com", get_block: true }, /* ws (plain) also works */
+            // check!  { url: "https://rpc.steemviz.com", get_block: true }, // Could not find method get_accounts
+
+            // discontinued: { url: "http://steemd.steemit.com", get_block: true }, { url: "https://steemd.steemit.com", get_block: true }, { url: "ws://steemd.steemit.com", get_block: true }, { url: "wss://steemd.steemit.com", get_block: true },
+            // inaccessible { url: "http://node.steem.ws ", get_block: true }, { url: "https://node.steem.ws ", get_block: true }, { url: "ws://node.steem.ws ", get_block: true }, { url: "wss://node.steem.ws ", get_block: true },
+            // inaccessible { url: "http://this.piston.rocks", get_block: true }, { url: "https://this.piston.rocks", get_block: true }, { url: "ws://this.piston.rocks", get_block: true }, { url: "wss://this.piston.rocks", get_block: true },
+            // 301: { url: "http://seed.bitcoiner.me", get_block: true }, { url: "https://seed.bitcoiner.me", get_block: true }, { url: "ws://seed.bitcoiner.me", get_block: true }, { url: "wss://seed.bitcoiner.me", get_block: true },
+            // 502: { url: "http://appbasetest.timcliff.com", get_block: true }, { url: "https://appbasetest.timcliff.com", get_block: true }, { url: "ws://appbasetest.timcliff.com", get_block: true }, { url: "wss://appbasetest.timcliff.com", get_block: true },
+            // inaccessible: { url: "http://appbase.buildteam.io", get_block: true }, { url: "https://appbase.buildteam.io", get_block: true }, { url: "ws://appbase.buildteam.io", get_block: true }, { url: "wss://appbase.buildteam.io", get_block: true },
+        ]
+    };
+
     witness = {
         account: "wise-team"
     };
@@ -33,6 +60,59 @@ export class Config {
             generateHelpUsMd: (data: any) => "" ,
             generateHelpMd: (data: any) => "",
             generateDefaultBadges: (data: any) => "",
+        },
+        repositories: {
+            core: {
+                name: "steem-wise-core",
+                npmPackageName: "steem-wise-core",
+                isNode: true,
+                isNpm: true,
+                nodePath: ""
+            },
+            cli: {
+                name: "steem-wise-cli",
+                isNode: true,
+                npmPackageName: "steem-wise-cli",
+                isNpm: true,
+                nodePath: ""
+            },
+            voterPage: {
+                name: "steem-wise-voter-page",
+                isNode: false,
+                isNpm: true,
+                nodePath: ""
+            },
+            manual: {
+                name: "steem-wise-manual",
+                isNode: false,
+                isNpm: false,
+                nodePath: ""
+            },
+            sql: {
+                name: "steem-wise-sql",
+                isNode: true,
+                isNpm: true,
+                nodePath: "/pusher"
+            },
+            test: {
+                name: "steem-wise-test",
+                isNode: true,
+                isNpm: true,
+                nodePath: ""
+            },
+            hub: {
+                name: "../wise-hub",
+                isNode: true,
+                isNpm: true,
+                nodePath: "/wise-hub-frontend"
+            },
+            ci: {
+                name: "wise-ci",
+                isNode: true,
+                isNpm: true,
+                nodePath: ""
+            }
+
         }
     };
 
@@ -44,6 +124,10 @@ export class Config {
     };
 
     sql = {
+        pusher: {
+            requestConcurrencyPerNode: 3,
+            blockProcessingTimeoutMs: 9000
+        },
         endpoint: { host: "sql.wise.vote", schema: "http" }
     };
 
@@ -55,6 +139,33 @@ export class Config {
         production: { url: "http://wise.vote/" },
         development: { url: "http://portal.wise.vote/" }
     };
+
+    test = {
+        live: {
+            metrics: {
+                periodMs: 3 * 24 * 3600 * 1000 // 3 days
+            },
+            inBrowserTests: {
+                enabled: true,
+                browsers: [ "firefox" ]
+            },
+            api: {
+                testThroughProxy: false
+            }
+        },
+        websites: {
+            brokenLinks: {
+                excludes: [ "*linkedin.com*", "http://sql.wise.vote/operations?select=moment,delegator,voter,operation_type&order=moment.desc" ]
+            },
+            forbiddenPhrases: [ "noisy-witness", "noisy witness", "smartvote", "muon"]
+        }
+    };
+
+    websites = [
+        { url: this.wise.homepage, checkBrokenLinks: true },
+        { url: this.team.website.url, checkBrokenLinks: true },
+        { url: this.manual.url, checkBrokenLinks: false /* it is a part of wise homepage */ },
+    ];
 }
 
 export const config = new Config();
